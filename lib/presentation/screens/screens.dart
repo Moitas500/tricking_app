04f3fd338f7981adc6202fe7cd39cc3e96ac9fca
Cristhian <camilofour1@gmail.com>
2026-03-13 17:23:02 -0500

    [ADD] create file screens.dart like a container

---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,8 @@
+export 'package:tricking_app/presentation/screens/combo_generator/combo_generator_screen.dart';
+export 'package:tricking_app/presentation/screens/home/home_screen.dart';
+export 'package:tricking_app/presentation/screens/profile/profile_screen.dart';
+export 'package:tricking_app/presentation/screens/progress_analytics/progress_analytics_screen.dart';
+export 'package:tricking_app/presentation/screens/skill_tree/skill_tree_screen.dart';
+export 'package:tricking_app/presentation/screens/training_planner/training_planner_screen.dart';
+export 'package:tricking_app/presentation/screens/trick_glossary/trick_glossary_screen.dart';
+export 'package:tricking_app/presentation/screens/video_library/video_library_screen.dart';
