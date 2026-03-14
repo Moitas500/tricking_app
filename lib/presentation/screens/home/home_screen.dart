a73249c7e1330e1fe1332d88b2133b60704dff9a
Cristhian <camilofour1@gmail.com>
2026-03-14 18:24:19 -0500

    [ADD] rename again header

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tricking_app/presentation/widgets/header/header.dart';
+import 'package:tricking_app/presentation/screens/home/widgets/header.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String name = 'home_screen';
