c19c1524c5f9bb32f48890503bd5f61d90c1bd7f
Cristhian <camilofour1@gmail.com>
2026-03-14 18:13:49 -0500

    [ADD] add structure to home_screen

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,6 +8,41 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color(0xFF111827),
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.fitness_center), label: "Training"),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: "Progress"),
+          BottomNavigationBarItem(icon: Icon(Icons.group), label: "Community"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: const [
+                // HeaderSection(),
+                SizedBox(height: 20),
+                // LevelProgress(),
+                SizedBox(height: 20),
+                // SkillProgress(),
+                SizedBox(height: 20),
+                // TrainingStreak(),
+                SizedBox(height: 20),
+                // QuickActions(),
+                SizedBox(height: 20),
+                // RecentTraining(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
